32f021e430431d015f4184c2291300a04041e767
josepg <josepg@gmail.com>
2020-05-27 12:09:59 +0200

    Eliminar paràmetres obsolets (versió aules)

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 usuariAules = "DniAcí"
 contrasenyaAules = "contrasenyaAcí"
-NumAules = "2" # posar 2 per a aules2, deixar en blanc per a aules (""), etc
 cursosExclosos = set([123, 234])
