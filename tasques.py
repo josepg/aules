86a1534a654a3becff14176a2a5c2dd3093cd6af
josepg <josepg@gmail.com>
2020-05-31 16:54:27 +0200

    Afegir comprovació error d'inici de sessió

--- tasques.py
+++ tasques.py
@@ -9,7 +9,7 @@ elif major_version == 3:
 	import http.cookiejar as cookielib
 colorama.init()
 
-cprint("Benvinguts a l'script de tasques d'aules. Versió 1.1. (twitter: @josep_g)", 'green', 'on_grey')
+cprint("Benvinguts a l'script de tasques d'aules. Versió 1.2. (twitter: @josep_g)", 'green', 'on_grey')
 
 #Això evita errors amb els certificats autosignats d'aules:
 try:
@@ -23,13 +23,17 @@ br = mechanize.Browser() # Inicialitzem el navegador
 br.set_cookiejar(cookielib.LWPCookieJar()) # Assignem un contenidor per a les cookies (navegador)
 br.set_handle_robots(False) # Li indiquem a mechanize que ignore l'arxiu robots.txt que impeteix l'ús de robots.
 
-br.open("https://aules.edu.gva.es/moodle/login/index.php") # Obrim la pàgina d'inici d'aules configurada a l'arxiu de configuració
+br.open("https://aules.edu.gva.es/moodle/login/index.php") # Obrim la pàgina d'inici d'aules.
 
 br.select_form(nr=1) # Seleccionem el formulari d'inici de sessió
 br.form['username'] = config.usuariAules # Escrivim el nom d'usuari en el formulari d'inici de sessió
 br.form['password'] = config.contrasenyaAules # Escrivim la contrasenya en el formulari d'inici de sessió
 response = br.submit(id="loginbtn") # Enviem el formulari d'inici de sessió
 returnPage = response.read().decode('utf-8') # Resposta del formulari, pàgina inicial amb el llistat de cursos
+
+if 'loginerrormessage' in returnPage: # Si aules dona algún error (normalment usuari/contrasenya incorrectes) d'inici de sessió, eixim i avisem
+	cprint("Error iniciant la sessió en aules: heu configurat correctament l'usuari i la contrasenya?", 'red', 'on_white')
+	sys.exit()
 
 totalTasks = 0
 
